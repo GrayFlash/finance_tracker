8200bd75c4e2472bd9dcf814368687907b7ecd7d
Mustafa2856 <officewalamustafa@gmail.com>
2020-12-13 09:57:05 +0530

    text-classifier

--- expo/ML/index.js
+++ expo/ML/index.js
@@ -1,41 +1,54 @@
+const { data } = require('@tensorflow/tfjs');
 
-
-
-const webcamElement = document.getElementById('webcam');
+tf = require('@tensorflow/tfjs');
+knnClassifier = require('@tensorflow-models/knn-classifier');
+fs = require('fs')
+const model = tf.sequential();
+model.add(tf.layers.embedding({inputDim:100,outputDim:64}));
 const classifier = knnClassifier.create();
-
-async function app() {
-  console.log('Loading mobilenet..');
-  net = await mobilenet.load();
-  console.log('Successfully loaded model');
-  const webcam = await tf.data.webcam(webcamElement);
-  const addExample = async classId => {
-    const img = await webcam.capture();
-    const activation = net.infer(img, true);
-    classifier.addExample(activation, classId);
-    console.log('example added');
-    img.dispose();
-  };
-  document.getElementById('phone').addEventListener('click', () => addExample(0));
-  document.getElementById('mouse').addEventListener('click', () => addExample(1));
-  document.getElementById('face').addEventListener('click', () => addExample(2));
-
-  while (true) {
-    if (classifier.getNumClasses() > 0) {
-      const img = await webcam.capture();
-      const activation = net.infer(img, 'conv_preds');
-      const result = await classifier.predictClass(activation);
-
-      const classes = ['A', 'B', 'C'];
-      document.getElementById('console').innerText = `
-        prediction: ${classes[result.label]}\n
-        probability: ${result.confidences[result.label]}
-      `;
-      img.dispose();
+let points;
+fs.readFile('dat.json',{flag:'r'},(err,data) => {points = JSON.parse(data + "[0,0]]");addEx();});
+const addExample = (name,id) => {
+    t = [...name];
+    for(i=0;i<name.length;i++){
+        t[i]=t[i].charCodeAt();
+        if(t[i]>=65 && t[i]<=90)t[i]+=32;
+        else if(t[i]>=97 && t[i]<=122)t[i]+=0;
+        else t[i]=96;
+        t[i]-=96;
     }
-
-    await tf.nextFrame();
-  }
+    for(i=name.length;i<100;i++){
+        t[i]=0;
+    }
+    fs.writeFile('dat.json',JSON.stringify([t,id])+',',{flag:'a'},(err)=>{});
+    classifier.addExample(model.predict(tf.tensor(t,[1,100])),id)
 }
 
-app();
+const addEx = () => {
+    for(i=0;i<points.length;i++)
+    try{
+        classifier.addExample(model.predict(tf.tensor(points[i][0],[1,100])),points[i][1]);
+    }catch(err){console.log('err');}
+    predict('bourbon biscuit').then((obj)=>console.log(obj));
+predict('fromal shirt').then((obj)=>console.log(obj));
+predict('frying pan').then((obj)=>console.log(obj));
+predict('amul butter').then((obj)=>console.log(obj));
+predict('clinic plus shampoo').then((obj)=>console.log(obj));
+}
+
+const predict = async (name) => {
+    t = [...name];
+    for(i=0;i<name.length;i++){
+        t[i]=t[i].charCodeAt();
+        if(t[i]>=65 && t[i]<=90)t[i]+=32;
+        else if(t[i]>=97 && t[i]<=122)t[i]+=0;
+        else t[i]=96;
+        t[i]-=96;
+    }
+    for(i=name.length;i<100;i++){
+        t[i]=0;
+    }
+    return classifier.predictClass(model.predict(tf.tensor(t,[1,100]))).then((obj) => {
+        return obj;
+    });
+}
