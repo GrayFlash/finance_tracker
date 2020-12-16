f89d303d9af27e6e156112317a59e6aa1044eff8
neelbavarva <neelbavarva5@gmail.com>
2020-12-17 03:18:29 +0530

    VictoryLine Charts added

--- expo/screens/Stock.js
+++ expo/screens/Stock.js
@@ -1,7 +1,16 @@
 import { useNavigation } from '@react-navigation/native';
 import React, {useState, useEffect} from 'react';
-import { KeyboardAvoidingView, View , Text, StyleSheet, Image, TouchableOpacity, TextInput, Alert, FlatList, ScrollView, ImageBackground } from 'react-native';
+import { KeyboardAvoidingView, View , Text, StyleSheet, Image, TouchableOpacity, TextInput, Alert, FlatList, ScrollView, ImageBackground, Dimensions} from 'react-native';
 import * as myConstClass from './HttpLink';
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph
+  } from 'react-native-chart-kit';
+  import { VictoryBar, VictoryChart, VictoryTheme, VictoryLine } from "victory-native";
+
 
 export default function Stock() {
     
@@ -27,6 +36,44 @@ export default function Stock() {
                     </View>
                 </View>
             </View>
+
+            <Text style={{  margin: 10, fontSize: 18,fontFamily: 'GothamMedium', color: "black"}}>VictoryLine Charts</Text>
+
+            <VictoryChart theme={VictoryTheme.material}>
+                <VictoryLine
+                    style={{
+                    data: { stroke: "#c43a31" },
+                    parent: { border: "1px solid #ccc"}
+                    }}
+                    data={[
+                    { x: 1, y: 2 },
+                    { x: 2, y: 3 },
+                    { x: 3, y: 5 },
+                    { x: 4, y: 4 },
+                    { x: 5, y: 7 }
+                    ]}
+                    
+                />
+            </VictoryChart>
+
+            <VictoryChart theme={VictoryTheme.material}>
+                <VictoryLine
+                interpolation="natural"
+                    style={{
+                    data: { stroke: "#c43a31" },
+                    parent: { border: "1px solid #ccc"}
+                    }}
+                    data={[
+                    { x: 1, y: 2 },
+                    { x: 2, y: 3 },
+                    { x: 3, y: 5 },
+                    { x: 4, y: 4 },
+                    { x: 5, y: 7 }
+                    ]}
+                    
+                />
+            </VictoryChart>
+
         </ScrollView>
         
     );
