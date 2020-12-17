e970be02f5aa4734b0f064e256adfc3513eadcbe
neelbavarva <neelbavarva5@gmail.com>
2020-12-17 21:54:40 +0530

    sensex and nifty added

--- expo/screens/HttpLink.js
+++ expo/screens/HttpLink.js
@@ -1,1 +1,1 @@
-export const HTTP_LINK = "http://fa0ff0a41c2c.ngrok.io"
+export const HTTP_LINK = "http://ec0780797b5b.ngrok.io"
--- expo/screens/Stock.js
+++ expo/screens/Stock.js
@@ -14,27 +14,120 @@ import { VictoryBar, VictoryChart, VictoryTheme, VictoryLine } from "victory-nat
 export default function Stock() {
     
     const navigation = useNavigation();
-    const [result_stock, setResult_stock] = useState({ value:"" , raise: "", raisepercent: ''});
+    const [result_sensex, setResult_sensex] = useState({ value:"" , raise: "", raisepercent: ''});
+    const [result_nifty, setResult_nifty] = useState({ value:"" , raise: "", raisepercent: ''});
+    const [viewMode, setViewMode] = useState("rates");
 
-    const fetchStock = () => {
+
+    const fetchStock_sensex = () => {
         //let y = fetchExpense();
-        fetch(`${myConstClass.HTTP_LINK}/stock`)
+        fetch(`${myConstClass.HTTP_LINK}/sensex`)
         .then(res=>res.json())
         .then(result_stock=>{
             console.log("People data received inside Stock Page")
-            setResult_stock(result_stock);
-            console.log(result_stock);
+            setResult_sensex(result_stock);
+            // console.log(result_stock);
+        })
+    }
+
+    const fetchStock_nifty = () => {
+        //let y = fetchExpense();
+        fetch(`${myConstClass.HTTP_LINK}/nifty`)
+        .then(res=>res.json())
+        .then(result_stock=>{
+            console.log("People data received inside Stock Page")
+            setResult_nifty(result_stock);
+            // console.log(result_stock);
         })
     }
 
     useEffect(()=>{
-        fetchStock(),
+        fetchStock_sensex(),
+        fetchStock_nifty(),
         LogBox.ignoreLogs(['VirtualizedLists should never be nested']);
     },[])
+
+    const LineDivider = () => {
+        return (
+            <View style={{ width: 0, paddingVertical: 18 }}>
+                <View style={{ flex: 1, borderLeftColor: "gray", borderLeftWidth: 1 }}></View>
+            </View>
+        );
+    }
+
+    function Rates() {
+        return(
+            <View>
+                <Text style={{  marginLeft: 16, paddingTop: 16, fontSize: 18,fontFamily: 'GothamMedium', color: "black"}}>BSE SENSEX</Text>
+                <Text style={{  marginLeft: 16, paddingTop: 8, fontSize: 14,fontFamily: 'GothamLight', color: "black"}}>INDEXBOM: SENSEX</Text>
+
+                <View style={{ flexDirection: 'row'}}>
+                    
+                    <View style={{ flex: 1, marginLeft: 16, paddingTop: 26, flexDirection: 'row', alignItems: 'center' }}>
+                        <Text style={{fontFamily: 'GothamMedium', fontSize: 16, color: "black"}}>Value: {result_sensex.value.toString()}</Text>
+                    </View>
+
+                    <View style={{paddingTop: 26,marginRight: 16, justifyContent: 'center' }}>
+                        <Text style={{fontFamily: 'GothamMedium', fontSize: 16, color: result_sensex.raise.toString().charAt(0) == "+" ? "green" : "red"}}>{result_sensex.raise.toString()} {result_sensex.raisepercent.toString()}</Text>
+                    </View>
+                </View> 
+
+                <VictoryChart theme={VictoryTheme.material}>
+                    <VictoryLine
+                        style={{
+                        data: { stroke: "#c43a31" },
+                        parent: { border: "1px solid #ccc"}
+                        }}
+                        data={[
+                            { x: 1, y: 2 },
+                            { x: 2, y: 3 },
+                            { x: 3, y: 5 },
+                            { x: 4, y: 4 },
+                            { x: 5, y: 7 }
+                        ]}
+                    />
+                </VictoryChart>
+
+
+                <Text style={{  marginLeft: 16, paddingTop: 16, fontSize: 18,fontFamily: 'GothamMedium', color: "black"}}>NIFTY 50</Text>
+                <Text style={{  marginLeft: 16, paddingTop: 8, fontSize: 14,fontFamily: 'GothamLight', color: "black"}}>INDEX: NIFTY_50</Text>
+
+                <View style={{ flexDirection: 'row'}}>
+                    
+                    <View style={{ flex: 1, marginLeft: 16, paddingTop: 26, flexDirection: 'row', alignItems: 'center' }}>
+                        <Text style={{fontFamily: 'GothamMedium', fontSize: 16, color: "black"}}>Value: {result_nifty.value.toString()}</Text>
+                    </View>
+
+                    <View style={{paddingTop: 26,marginRight: 16, justifyContent: 'center' }}>
+                        <Text style={{fontFamily: 'GothamMedium', fontSize: 16, color: result_nifty.raise.toString().charAt(0) == "+" ? "green" : "red"}}>{result_nifty.raise.toString()} {result_nifty.raisepercent.toString()}</Text>
+                    </View>
+                </View>
+
+                <VictoryChart theme={VictoryTheme.material}>
+                    <VictoryLine
+                    interpolation="natural"
+                        style={{
+                        data: { stroke: "#c43a31" },
+                        parent: { border: "1px solid #ccc"}
+                        }}
+                        data={[
+                        { x: 1, y: 2 },
+                        { x: 2, y: 3 },
+                        { x: 3, y: 5 },
+                        { x: 4, y: 4 },
+                        { x: 5, y: 7 }
+                        ]}
+                        
+                    />
+                </VictoryChart>
+            </View>
+        )
+    }
     
     return (
 
         <ScrollView>
+
             <View style={{ flex: 1, justifyContent: 'center', backgroundColor: "white"}}>
                 <View style={{ flexDirection: 'row', marginTop: 40 , marginBottom: 10}}>
                     <TouchableOpacity style={{ flex: 1,}} onPress={() => navigation.goBack()} >
@@ -53,45 +146,79 @@ export default function Stock() {
                 </View>
             </View>
 
+            {/* NAVBAR */}
 
-            {/* THAKKER AAIYA PRINT KAR */}
-            <Text style={{  margin: 10, fontSize: 18,fontFamily: 'GothamMedium', color: "black"}}>Value: {result_stock.value.toString()} </Text>
-            <Text style={{  margin: 10, fontSize: 18,fontFamily: 'GothamMedium', color: "black"}}>Raise: {result_stock.raise.toString()} </Text>
-            <Text style={{  margin: 10, fontSize: 18,fontFamily: 'GothamMedium', color: "black"}}>raisePercent: {result_stock.raisepercent.toString()} </Text>
+            <View style={{ flex: 1, justifyContent: 'center', padding: 12,}}>
+                <View style={{ flexDirection: 'row', height: 45, backgroundColor: "white", borderRadius: 8 }}>
+                    {/* Claim */}
+                    <TouchableOpacity
+                        style={{ 
+                            flex: 1,
+                            backgroundColor: viewMode == "rates" ? "#BEC1D2" : null, 
+                            margin: 5,
+                            borderRadius: 6,
+                        }}
+                        onPress={() => setViewMode("rates")}
+                        
+                    >
+                        <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                            <Text style={{  marginLeft: 0, fontSize: 16,fontFamily: 'GothamMedium', lineHeight: 22, color: "black"}}>Rates</Text>
+                        </View>
+                    </TouchableOpacity>
 
-            <VictoryChart theme={VictoryTheme.material}>
-                <VictoryLine
-                    style={{
-                    data: { stroke: "#c43a31" },
-                    parent: { border: "1px solid #ccc"}
-                    }}
-                    data={[
-                        { x: 1, y: 2 },
-                        { x: 2, y: 3 },
-                        { x: 3, y: 5 },
-                        { x: 4, y: 4 },
-                        { x: 5, y: 7 }
-                    ]}
-                />
-            </VictoryChart>
+                    {/* Divider */}
+                    <LineDivider />
 
-            <VictoryChart theme={VictoryTheme.material}>
-                <VictoryLine
-                interpolation="natural"
-                    style={{
-                    data: { stroke: "#c43a31" },
-                    parent: { border: "1px solid #ccc"}
-                    }}
-                    data={[
-                    { x: 1, y: 2 },
-                    { x: 2, y: 3 },
-                    { x: 3, y: 5 },
-                    { x: 4, y: 4 },
-                    { x: 5, y: 7 }
-                    ]}
-                    
-                />
-            </VictoryChart>
+                    {/* Get Point */}
+                    <TouchableOpacity
+                        style={{ 
+                            flex: 1,
+                            backgroundColor: viewMode == "sensex" ? "#BEC1D2" : null, 
+                            margin: 5,
+                            borderRadius: 8,
+                        }}
+                        onPress={() => setViewMode("sensex")}
+                    >
+                        <View
+                            style={{
+                                flex: 1,
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <Text style={{ marginLeft: 0, fontSize: 16, fontFamily: 'GothamMedium', lineHeight: 22, color: "black" }}>SenSex</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    {/* Divider */}
+                    <LineDivider />
+
+                    {/* My Card */}
+                    <TouchableOpacity
+                        style={{ 
+                            flex: 1,
+                            backgroundColor: viewMode == "nifty" ? "#BEC1D2" : null, 
+                            margin: 5,
+                            borderRadius: 8,
+                        }}
+                        onPress={() => setViewMode("nifty")}
+                    >
+                        <View
+                            style={{
+                                flex: 1,
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <Text style={{ marginLeft: 0, fontSize: 16, fontFamily: 'GothamMedium', lineHeight: 22, color: "black" }}>Nifty 50</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            
+            { viewMode == "rates" && Rates()}
 
         </ScrollView>
         
