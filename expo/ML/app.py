364904fa71a840b2eb4883125d82d91cdc43f7f0
GrayFlash <gk74533@gmail.com>
2020-12-20 15:51:14 +0530

    Add final Python Part for OCR, Prediction

--- expo/ML/app.py
+++ expo/ML/app.py
@@ -36,17 +36,17 @@ def detect_text(path):
     response = client.text_detection(image=image)
     texts = response.text_annotations
     
-    print('Texts:')
+    # print('Texts:')
     
     vertices = []
     for text in texts:
-        print('\n"{}"'.format(text.description))
+        # print('\n"{}"'.format(text.description))
         li = []
         for vertex in text.bounding_poly.vertices:
             li.append([vertex.x, vertex.y])
         words.append(text.description)
         poly_bounds.append(li)
-        print(li)
+        # print(li)
     if response.error.message:
         raise Exception(
             '{}\nFor more info on error messages, check: '
@@ -54,11 +54,97 @@ def detect_text(path):
                 response.error.message))
     return texts, words, poly_bounds
 
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
 def find_content_from_output(texts, words, poly_bounds):
     st = ""
     n = 0
+    count_words = 0
+    out = []
 
-    return st, n
+    for word in words:
+        
+        if count_words == 0:
+            count_words += 1
+            continue
+        
+        elif count_words == 1:
+            out.append([word, poly_bounds[count_words]])
+            count_words += 1
+        else:
+            [[x_a, y_a], [x_b, y_b], [x_c, y_c], [x_d, y_d]] = poly_bounds[count_words]
+            count_words += 1
+            flag = 0
+            for i in out:
+                [[x_a1, y_a1], [x_b1, y_b1], [x_c1, y_c1], [x_d1, y_d1]] = i[1]
+                if flag == 1:
+                    break
+                    
+                if abs(y_a1 - y_a)>15:
+                    continue
+                
+                else:
+                    flag = 1
+                    if x_b1 < x_a:
+                        temp_word = i[0]
+                        if x_a-x_b1 > 30:
+                            temp_word = temp_word +"\t"+ word
+                            i[0] = temp_word
+                            i[1] = [[x_a1, y_a1], [x_b, y_b], [x_c, y_c], [x_a1, y_a1]]
+                        elif x_a-x_b1 > 7:
+                            temp_word = temp_word +" "+ word
+                            i[0] = temp_word
+                            i[1] = [[x_a1, y_a1], [x_b, y_b], [x_c, y_c], [x_a1, y_a1]]
+                        else:
+                            temp_word = temp_word +""+ word
+                            i[0] = temp_word
+                            i[1] = [[x_a1, y_a1], [x_b, y_b], [x_c, y_c], [x_a1, y_a1]]
+                        
+                    else:
+                        temp_word = i[0]
+                        if x_b1-x_a > 30:
+                            temp_word = word +"\t"+ temp_word
+                            i[0] = temp_word
+                            i[1] = [[x_a, y_a], [x_b1, y_b1], [x_c1, y_c1], [x_a, y_a]]
+                        elif x_b1-x_a > 7:
+                            temp_word = word +" "+ temp_word
+                            i[0] = temp_word
+                            i[1] = [[x_a, y_a], [x_b1, y_b1], [x_c1, y_c1], [x_a, y_a]]
+                        else:
+                            temp_word = word +""+ temp_word
+                            i[0] = temp_word
+                            i[1] = [[x_a, y_a], [x_b1, y_b1], [x_c1, y_c1], [x_a, y_a]]
+            
+            if flag == 0:
+                out.append([word, poly_bounds[count_words-1]])
+    
+    products_name = []
+    categories = []
+    prices = []
+    for i in out:
+        x = i[0].split("\t")
+        #print(x)
+        
+        item = ""
+        price = 0
+        for j in x:
+            if is_number(j) == True:
+                price = float(j)
+            else:
+                item = item+" "+j
+        item.strip(" ")
+        # print(item, price)
+        if item!="" and price>1:
+            products_name.append(item)
+            prices.append(price)
+    if len(products_name)>=1:
+        categories = predict(products_name)
+    return products_name, categories, prices
 
 @app.route("/image_ocr", methods=["POST"])
 def ocr_enable():
@@ -81,7 +167,8 @@ def ocr_enable():
 
     texts, words, poly_bounds = detect_text("Bill.jpg")
 
-    st, n = find_content_from_output(texts, words, poly_bounds)
-
-    print(texts)
+    names, categories, prices = find_content_from_output(texts, words, poly_bounds)
+    for i in range(0, len(names)):
+        print(names[i], categories[i], prices[i])
+    # print(texts)
     return data
