ae92902d71d63f358d8f25e9ae5682d9706857d1
GrayFlash <gk74533@gmail.com>
2020-12-20 11:15:51 +0530

    Resolved package conficts in env, moved to google.cloud package

--- expo/ML/app.py
+++ expo/ML/app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import requests
 from PIL import Image, ImageEnhance
-# from google.cloud import vision
+from google.cloud import vision
 import io
 import cv2 as cv
 from flask import Flask, request, jsonify
@@ -20,6 +20,40 @@ def prediction():
     print(res)
     return {"category":res[0]}
 
+def detect_text(path):
+    """Detects text in the file."""
+    
+    client = vision.ImageAnnotatorClient()
+
+    words = []
+    poly_bounds = []
+    
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    
+    print('Texts:')
+    
+    vertices = []
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+        li = []
+        for vertex in text.bounding_poly.vertices:
+            li.append([vertex.x, vertex.y])
+        words.append(text.description)
+        poly_bounds.append(li)
+        print(li)
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    return texts, words, poly_bounds
+
 @app.route("/image_ocr", methods=["POST"])
 def ocr_enable():
     data = []
@@ -36,4 +70,7 @@ def ocr_enable():
     img2 = cv.imread("Bill.jpg", 0)
     th = cv.adaptiveThreshold(img2,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
     cv.imwrite("Bill.jpg", th)
+
+    texts, words, poly_bounds = detect_text("Bill.jpg")
+    print(texts)
     return data
